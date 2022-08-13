96c7e6995e410dd7e66f3546fdb2145a425a092a
Bojan Golic <bokigolic32@gmail.com>
2022-08-13 00:52:45 -0400

    add preset and initial state

--- delete-example-/src/components/DeleteExample.jsx
+++ delete-example-/src/components/DeleteExample.jsx
@@ -1,10 +1,49 @@
-const DeleteExample = () =>{
+import { useState } from "react";
+const DeleteExample = () => {
+  const preset = [
+    {
+      id: 1,
+      name: "Tom Cruise",
+      movie: "Top Gun"
+    },
+    {
+      id: 2,
+      name: "Matt Damon",
+      movie: "Good Will Hunting"
+    },
+    {
+      id: 3,
+      name: "Will Smith",
+      movie: "Pirates of the Caribbean",
+    },
+    {
+      id: 4,
+      name: "Samuel L. Jackson",
+      movie: "Shaft"
+    },
+    {
+      id: 5,
+      name: "Marlon Brando",
+      movie: "Apocalypse Now"
+    },
+    {
+      id: 6,
+      name: "Robert De Niro",
+      movie: "Intern",
+    }
+  ]
+
+  const [movies, setMovies] = useState(preset)
 
   return (
-    <div>
-   <h1>Delete example</h1>
-    </div>
-      )
+    <form>
+      <div>
+        <h1>Delete example</h1>
+
+
+      </div>
+    </form>
+  )
 
 }
 
